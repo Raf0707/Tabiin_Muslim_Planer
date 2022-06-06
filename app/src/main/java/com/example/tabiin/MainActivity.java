5b5f0a9d6189227c4e7c39de8b090051401d09d7
Raf <Raf_0707@mail.ru>
2022-06-06 13:33:29 +0300

    create new folders impliment View.Binding in gradle and MainActivity

--- app/src/main/java/com/example/tabiin/MainActivity.java
+++ app/src/main/java/com/example/tabiin/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.tabiin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SimpleAdapter.ViewBinder {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean setViewValue(View view, Object o, String s) {
+        return false;
     }
 }
