3f3c7d3926df1e34cc88d0c4aad2cc2d51de83e4
Raf <Raf_0707@mail.ru>
2022-06-08 23:05:12 +0300

    create new design for Counter.java create new colors, items, icons, textures create View view = inflater.inflate(R.layout.fragment_counter, container, false); and return view; ppcc - Progress Bar circle (round) corners View separator.xml

--- app/src/main/java/com/example/tabiin/fragments/Counter.java
+++ app/src/main/java/com/example/tabiin/fragments/Counter.java
@@ -60,7 +60,9 @@ public class Counter extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_counter, container, false);
+
+        View view =  inflater.inflate(R.layout.fragment_counter, container, false);
+
+        return view;
     }
 }
