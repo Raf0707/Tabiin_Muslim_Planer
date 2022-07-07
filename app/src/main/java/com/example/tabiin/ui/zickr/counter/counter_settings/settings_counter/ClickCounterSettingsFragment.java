fdaf6e776e5ca88da2db596c0a0a0a3dc240ea08
Raf <Raf_0707@mail.ru>
2022-07-07 05:58:15 +0300

    all counter's settings

--- app/src/main/java/com/example/tabiin/ui/zickr/counter/counter_settings/settings_counter/ClickCounterSettingsFragment.java
+++ app/src/main/java/com/example/tabiin/ui/zickr/counter/counter_settings/settings_counter/ClickCounterSettingsFragment.java
@@ -1,4 +1,4 @@
-package com.example.tabiin.ui.zickr.gesture_counter;
+package com.example.tabiin.ui.zickr.counter.counter_settings.settings_counter;
 
 import android.os.Bundle;
 
@@ -12,12 +12,10 @@ import com.example.tabiin.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link GestureCounterFragment#newInstance} factory method to
+ * Use the {@link ClickCounterSettingsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class GestureCounterFragment extends Fragment {
-
-    //android:src="@drawable/tap"
+public class ClickCounterSettingsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,7 +26,7 @@ public class GestureCounterFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public GestureCounterFragment() {
+    public ClickCounterSettingsFragment() {
         // Required empty public constructor
     }
 
@@ -38,11 +36,11 @@ public class GestureCounterFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment GestureCounter.
+     * @return A new instance of fragment ClickCounterSettingsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static GestureCounterFragment newInstance(String param1, String param2) {
-        GestureCounterFragment fragment = new GestureCounterFragment();
+    public static ClickCounterSettingsFragment newInstance(String param1, String param2) {
+        ClickCounterSettingsFragment fragment = new ClickCounterSettingsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -62,10 +60,7 @@ public class GestureCounterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_gesture_counter, container, false);
-
-
-
-        return view;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_click_counter_settings, container, false);
     }
 }
--- app/src/main/java/com/example/tabiin/ui/zickr/gesture_counter/general_screen/GestureCounterFragment.java
+++ app/src/main/java/com/example/tabiin/ui/zickr/gesture_counter/general_screen/GestureCounterFragment.java
@@ -1,4 +1,4 @@
-package com.example.tabiin.ui.zickr.counter.counter_settings;
+package com.example.tabiin.ui.zickr.gesture_counter.general_screen;
 
 import android.os.Bundle;
 
@@ -12,10 +12,12 @@ import com.example.tabiin.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link GeneralCounterSettingsFragment#newInstance} factory method to
+ * Use the {@link GestureCounterFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class GeneralCounterSettingsFragment extends Fragment {
+public class GestureCounterFragment extends Fragment {
+
+    //android:src="@drawable/tap"
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +28,7 @@ public class GeneralCounterSettingsFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public GeneralCounterSettingsFragment() {
+    public GestureCounterFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +38,11 @@ public class GeneralCounterSettingsFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment GeneralCounterSettingsFragment.
+     * @return A new instance of fragment GestureCounter.
      */
     // TODO: Rename and change types and number of parameters
-    public static GeneralCounterSettingsFragment newInstance(String param1, String param2) {
-        GeneralCounterSettingsFragment fragment = new GeneralCounterSettingsFragment();
+    public static GestureCounterFragment newInstance(String param1, String param2) {
+        GestureCounterFragment fragment = new GestureCounterFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -60,7 +62,10 @@ public class GeneralCounterSettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_general_counter_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_gesture_counter, container, false);
+
+
+
+        return view;
     }
 }
